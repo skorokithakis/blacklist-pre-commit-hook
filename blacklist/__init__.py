2bc72f34985d11fd21dcbd0995252f5476b7d4ef
Stavros Korokithakis <hi@stavros.io>
2018-06-14 16:30:34 +0300

    Fix call to super()

--- blacklist/__init__.py
+++ blacklist/__init__.py
@@ -18,7 +18,7 @@ class ASTVisitor(ast.NodeVisitor):
         self.ignore = set(ignore) if ignore else set()
 
     def visit_Call(self, node):
-        super().generic_visit(node)
+        super(self, visit_Call).generic_visit(node)
         func = node.func
 
         if hasattr(func, "attr"):
